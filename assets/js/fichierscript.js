d22483b22ee34c48b7eed988ec1e3c9dcf9c313c
remydegrave <bolieve.fr@gmail.com>
2020-11-09 11:29:21 +0100

    exercice1pt2Jq

---
+++ assets/js/fichierscript.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.img').mouseover(function(){
+            $('img').width(600);
+        });
+    $('.img').mouseout(function(){
+            $('img').width(300);
+        });
+});
