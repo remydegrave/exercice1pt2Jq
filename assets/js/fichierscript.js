e7cc970b34b565284ff28e53483dcce8610f36c4
remydegrave <bolieve.fr@gmail.com>
2020-11-12 14:34:39 +0100

    correction

--- assets/js/fichierscript.js
+++ assets/js/fichierscript.js
@@ -1,7 +1,9 @@
 $(function(){
+    // Pour agrandir l'image quand on vient dessus avec la souris.
     $('.img').mouseover(function(){
             $('img').width(600);
         });
+        // Pour réduire la taille de l'image quand la souris n'est pas dessus.
     $('.img').mouseout(function(){
             $('img').width(300);
         });
